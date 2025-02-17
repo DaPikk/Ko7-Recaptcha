ace5c706a5c09b1757c972f7bd4418db7dfd9385
DaPikk <dapikk@gmail.com>
2025-02-17 18:00:02 +0200

    Update recaptcha.php
    support for different reCaptcha versions

--- config/recaptcha.php
+++ config/recaptcha.php
@@ -3,6 +3,7 @@
 return array(
 	'public_key'  => 'YOUR GOOGLE RECAPTCHA SITE KEY',
 	'private_key' => 'YOUR GOOGLE RECAPTCHA SECRET KEY',
+	'version' => 'v2',
         'theme' => 'light',
 	'dlang' => 'en',
         'dsize' => 'normal',
