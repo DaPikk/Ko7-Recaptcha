7e0ce5bad642629c407040d3b7153b6e9d991e0e
DaPikk <dapikk@gmail.com>
2025-02-17 18:57:56 +0200

    Update recaptcha.php

--- config/recaptcha.php
+++ config/recaptcha.php
@@ -4,6 +4,7 @@ return array(
 	'public_key'  => 'YOUR GOOGLE RECAPTCHA SITE KEY',
 	'private_key' => 'YOUR GOOGLE RECAPTCHA SECRET KEY',
 	'version' => 'v2',
+	'rscore' => NULL, //Minimum score for safe actions, defaults to 0.5 and up
         'theme' => 'light',
 	'dlang' => 'en',
         'dsize' => 'normal',
